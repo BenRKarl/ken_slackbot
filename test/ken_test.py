c4c50f07fa17c02e944147fa4c3b9cf6daf76ca9
BenRKarl <benrkarl@gmail.com>
2016-11-15 21:58:09 -0500

    Add first test for Ken.get_user_name method

--- test/ken_test.py
+++ test/ken_test.py
@@ -1,11 +1,14 @@
 import unittest
 
-from ken import Ken
+from bot import ken
 
 class KenTests(unittest.TestCase):
 
   def test_get_user_name(self):
-    self.assertEqual(True, False)
+    test_bot = ken.Ken()
+    user_id = 'U0F19CBU2'
+    user_name = 'Ben'
+    self.assertEqual(test_bot.get_user_name(user_id), user_name)
 
 
 if __name__ == '__main__':
