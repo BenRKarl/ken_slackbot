111384deed74b20ea1beb02d6cb54d3f21ed5372
BenRKarl <benrkarl@gmail.com>
2016-11-15 21:48:28 -0500

    Add first failing test!

--- test/ken_test.py
+++ test/ken_test.py
@@ -1,1 +1,12 @@
+import unittest
+
 from ken import Ken
+
+class KenTests(unittest.TestCase):
+
+  def test_get_user_name(self):
+    self.assertEqual(True, False)
+
+
+if __name__ == '__main__':
+    unittest.main()
