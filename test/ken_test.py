1ef4270afbbb41382e5a87d2f6009cf94c012cc4
BenRKarl <benrkarl@gmail.com>
2016-11-20 17:08:01 -0500

    Add tests for getter/setter methods for Ken to store current user and room

--- test/ken_test.py
+++ test/ken_test.py
@@ -16,5 +16,31 @@ class KenTests(unittest.TestCase):
     user_name = 'Katie'
     self.assertEqual(test_bot.get_user_name(user_id), user_name)
 
+  def test_set_current_user(self):
+    test_bot = Ken()
+    test_name = 'Test'
+    test_bot.set_current_user(test_name)
+    self.assertEqual(test_bot.current_user, test_name)
+
+  def test_set_current_room(self):
+    test_bot = Ken()
+    test_room = 12345
+    test_bot.set_current_room(test_room)
+    self.assertEqual(test_bot.current_room, test_room)
+
+  def test_get_current_user(self):
+    test_bot = Ken()
+    test_user = 'Test'
+    test_bot.current_user = test_user
+    returned = test_bot.get_current_user()
+    self.assertEqual(returned, test_user)
+
+  def test_get_current_room(self):
+    test_bot = Ken()
+    test_room = 12345
+    test_bot.current_room = test_room
+    returned = test_bot.get_current_room()
+    self.assertEqual(returned, test_room)
+
 if __name__ == '__main__':
     unittest.main()
