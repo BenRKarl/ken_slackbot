6a449233459c4402e44a9a5663a6546603d74373
BenRKarl <benrkarl@gmail.com>
2016-11-20 16:55:46 -0500

    Move mocks to a new folder

--- test/parser_test.py
+++ test/parser_test.py
@@ -1,8 +1,7 @@
 import unittest
 import datetime
-import mocks
-
-from lib import parser
+from mocks import mock
+from helpers import parser
 
 class ParserTests(unittest.TestCase):
     def test_summate_empty_list(self):
@@ -10,17 +9,17 @@ class ParserTests(unittest.TestCase):
         self.assertEqual(total, 0)
 
     def test_summate_single_purchase(self):
-        purchase = mocks.single_purchase()
+        purchase = mock.single_purchase()
         total = parser.summate_purchases(purchase)
         self.assertEqual(total, 10.0)
 
     def test_summate_multiple_purchases(self):
-        purchases = mocks.multiple_purchases()
+        purchases = mock.multiple_purchases()
         total = parser.summate_purchases(purchases)
         self.assertEqual(total, 35.0)
 
     def test_filter_by_month(self):
-        purchases = mocks.purchases_in_different_months()
+        purchases = mock.purchases_in_different_months()
         month = 11
         filtered = parser.filter_by_month(purchases, month)
         self.assertEqual(len(filtered), 1)
