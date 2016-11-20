2c92000804c2d5db8bd2604b646edc3872a8f89d
BenRKarl <benrkarl@gmail.com>
2016-11-20 18:17:10 -0500

    Rename test method names to reflect methods on Ken

--- test/ken_test.py
+++ test/ken_test.py
@@ -10,26 +10,26 @@ class KenTests(unittest.TestCase):
     test_bot.set_current_user_id(user_id)
     self.assertEqual(test_bot.get_current_user_name(), 'Ben')
 
-  def test_set_current_user(self):
+  def test_set_current_user_id(self):
     test_bot = Ken()
     test_name = 'Test'
     test_bot.set_current_user_id(test_name)
     self.assertEqual(test_bot.current_user_id, test_name)
 
-  def test_set_current_channel(self):
+  def test_set_current_channel_id(self):
     test_bot = Ken()
     test_channel = 12345
     test_bot.set_current_channel_id(test_channel)
     self.assertEqual(test_bot.current_channel_id, test_channel)
 
-  def test_get_current_user(self):
+  def test_get_current_user_id(self):
     test_bot = Ken()
     test_user = 'Test'
     test_bot.current_user_id = test_user
     returned = test_bot.get_current_user_id()
     self.assertEqual(returned, test_user)
 
-  def test_get_current_channel(self):
+  def test_get_current_channel_id(self):
     test_bot = Ken()
     test_channel = 12345
     test_bot.current_channel_id = test_channel
