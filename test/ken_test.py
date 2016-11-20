342698a7f18c626919a2613e3d98a4d0d974f2ba
BenRKarl <benrkarl@gmail.com>
2016-11-20 12:57:24 -0500

    Add test that Ken knows Katies user ID

--- test/ken_test.py
+++ test/ken_test.py
@@ -4,10 +4,16 @@ from bot.ken import Ken
 
 class KenTests(unittest.TestCase):
 
-  def test_get_user_name(self):
+  def test_get_ben_user_name(self):
     test_bot = Ken()
     user_id = 'U0F19CBU2'
     user_name = 'Ben'
+    self.assertEqual(test_bot.get_user_name(user_id), user_name)
+
+  def test_get_katie_user_name(self):
+    test_bot = Ken()
+    user_id = 'U0F1FNUCQ'
+    user_name = 'Katie'
     self.assertEqual(test_bot.get_user_name(user_id), user_name)
 
 if __name__ == '__main__':
