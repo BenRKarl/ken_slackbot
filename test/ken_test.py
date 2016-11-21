67d50af0620f09b6a64005846c167ee3cbdc0fda
BenRKarl <benrkarl@gmail.com>
2016-11-20 20:06:56 -0500

    Refactor tests to use shared variables, add tests for determine_amount_owed

--- test/ken_test.py
+++ test/ken_test.py
@@ -1,33 +1,31 @@
 import unittest
-
 from bot.ken import Ken
+mock_user_id = 'U0F19CBU2'
+mock_channel_id = 12345
+mock_user_name = 'Test'
 
 class KenTests(unittest.TestCase):
 
   def test_get_current_user_name(self):
     test_bot = Ken()
-    user_id = 'U0F19CBU2'
-    test_bot.set_current_user_id(user_id)
+    test_bot.set_current_user_id(mock_user_id)
     self.assertEqual(test_bot.get_current_user_name(), 'Ben')
 
   def test_set_current_user_id(self):
     test_bot = Ken()
-    test_name = 'Test'
-    test_bot.set_current_user_id(test_name)
-    self.assertEqual(test_bot.current_user_id, test_name)
+    test_bot.set_current_user_id(mock_user_name)
+    self.assertEqual(test_bot.current_user_id, mock_user_name)
 
   def test_set_current_channel_id(self):
     test_bot = Ken()
-    test_channel = 12345
-    test_bot.set_current_channel_id(test_channel)
-    self.assertEqual(test_bot.current_channel_id, test_channel)
+    test_bot.set_current_channel_id(mock_channel_id)
+    self.assertEqual(test_bot.current_channel_id, mock_channel_id)
 
   def test_get_current_user_id(self):
     test_bot = Ken()
-    test_user = 'Test'
-    test_bot.current_user_id = test_user
+    test_bot.current_user_id = mock_user_id
     returned = test_bot.get_current_user_id()
-    self.assertEqual(returned, test_user)
+    self.assertEqual(returned, mock_user_id)
 
   def test_get_current_channel_id(self):
     test_bot = Ken()
--- test/parser_test.py
+++ test/parser_test.py
@@ -4,25 +4,32 @@ from mocks import mock
 from helpers import parser
 
 class ParserTests(unittest.TestCase):
-    def test_summate_empty_list(self):
-        total = parser.summate_purchases([])
-        self.assertEqual(total, 0)
+  def test_summate_empty_list(self):
+    total = parser.summate_purchases([])
+    self.assertEqual(total, 0)
 
-    def test_summate_single_purchase(self):
-        purchase = mock.single_purchase()
-        total = parser.summate_purchases(purchase)
-        self.assertEqual(total, 10.0)
+  def test_summate_single_purchase(self):
+    purchase = mock.single_purchase()
+    total = parser.summate_purchases(purchase)
+    self.assertEqual(total, 10.0)
 
-    def test_summate_multiple_purchases(self):
-        purchases = mock.multiple_purchases()
-        total = parser.summate_purchases(purchases)
-        self.assertEqual(total, 35.0)
+  def test_summate_multiple_purchases(self):
+    purchases = mock.multiple_purchases()
+    total = parser.summate_purchases(purchases)
+    self.assertEqual(total, 35.0)
 
-    def test_filter_by_month(self):
-        purchases = mock.purchases_in_different_months()
-        month = 11
-        filtered = parser.filter_by_month(purchases, month)
-        self.assertEqual(len(filtered), 1)
+  def test_filter_by_month(self):
+    purchases = mock.purchases_in_different_months()
+    month = 11
+    filtered = parser.filter_by_month(purchases, month)
+    self.assertEqual(len(filtered), 1)
+
+  def test_determine_amount_owed(self):
+    totals_list = [('Ben', 234), ('Katie', 120)]
+    user_name = 'Ben'
+    expected = ('Katie', 114)
+    actual = parser.determine_amount_owed(user_name, totals_list)
+    self.assertEqual(actual, expected)
 
 if __name__ == '__main__':
-    unittest.main()
+  unittest.main()
