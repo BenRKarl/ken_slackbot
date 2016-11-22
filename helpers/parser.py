26c1030fd452fa37275aa0c9df078b4aaa524bc4
BenRKarl <benrkarl@gmail.com>
2016-11-21 20:19:53 -0500

    Add get_debt_summary method to parser

--- helpers/parser.py
+++ helpers/parser.py
@@ -49,3 +49,10 @@ def get_debtor(totals_list):
       debtor = total
 
   return debtor
+
+def get_debt_summary(biggest_spender, debtor):
+  biggest_spender_name = biggest_spender[0]
+  debtor_name = debtor[0]
+  difference = biggest_spender[1] - debtor[1]
+  amount_owed = difference / 2
+  return (debtor_name, biggest_spender_name, amount_owed)
