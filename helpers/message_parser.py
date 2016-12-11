01d4c15471f3ed8cd986b32767a4ef441e0e6f79
BenRKarl <benrkarl@gmail.com>
2016-12-11 16:12:08 -0500

    Add tests for mew message_parser clean_command method and implmentation

--- helpers/message_parser.py
+++ helpers/message_parser.py
@@ -1,3 +1,9 @@
 def parse_purchase_amount(amount):
   cleaned = amount.replace("$", "")
   return float(cleaned)
+
+def clean_command(command):
+  if (command.startswith(':')):
+    return command[1:].strip()
+
+  return command.strip()
