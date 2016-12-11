ffdcf9a8bc0b340eb3439428c54e7f9826b9bb01
BenRKarl <benrkarl@gmail.com>
2016-12-11 16:08:42 -0500

    Add test for clean_command method that removes colons from the beginning of commands

--- test/message_parser_test.py
+++ test/message_parser_test.py
@@ -8,5 +8,11 @@ class MessageParserTests(unittest.TestCase):
     actual = message_parser.parse_purchase_amount(mock_amount)
     self.assertEqual(expected, actual)
 
+  def test_clean_command(self):
+    mock_command = ': test something : colon'
+    expected = 'test something : colon'
+    actual = message_parser.clean_command(mock_command)
+    self.assertEqual(expected, actual)
+
 if __name__ == '__main__':
   unittest.main()
